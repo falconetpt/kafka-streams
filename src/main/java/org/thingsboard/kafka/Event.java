c5e150c5cab830889be1484f06ed59fd838ac8d4
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-04-23 09:11:13 +0100

    Init

---
+++ src/main/java/org/thingsboard/kafka/Event.java
@@ -0,0 +1,18 @@
+package org.thingsboard.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Event {
+  private String provider;
+  private String paymentSubmissionId;
+  private String eventType;
+  private String status;
+  private String payload;
+}
---
+++ src/main/java/org/thingsboard/kafka/KafkaStreamsApp.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright © 2016-2019 The Thingsboard Authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.time.Duration;
+import java.util.Properties;
+
+@Slf4j
+public class KafkaStreamsApp {
+    private static final String IN_TOPIC = "payment_submissions";
+    private static final Serde<Integer> EVENT_SERDE =
+            Serdes.serdeFrom(new JsonPojoSerializer<>(), new JsonPojoDeserializer<>(Integer.class));
+    private static final Serde<String> STRING_SERDE = Serdes.String();
+
+    public static void main(final String[] args) {
+        runKafkaStreams();
+    }
+
+    private static void runKafkaStreams() {
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, Integer> source = builder
+            .stream(IN_TOPIC, Consumed.with(STRING_SERDE, EVENT_SERDE))
+            .map(KeyValue::pair)
+            .filter((k, v) -> v % 2 == 0);
+
+        source.foreach((k, v) -> log.info("even number {}", v));
+
+        log.info("STARTING");
+        final KafkaStreams streams = new KafkaStreams(builder.build(), getProperties());
+        streams.cleanUp();
+        streams.start();
+
+        log.info("STARTED");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private static Properties getProperties() {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka streams");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return props;
+    }
+}
